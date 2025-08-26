b2223f5042a5e9e4afb87301f3e585d7cb4d0aaf
David Torres <danthonyt@proton.me>
2025-08-25 17:57:47 -0700

    tested rx and tx

--- rtl/uart_rx.sv
+++ rtl/uart_rx.sv
@@ -7,112 +7,103 @@ module uart_rx #(
     // baud rate
     parameter CLKS_PER_BIT = 87
 ) (
-    input logic CLK,
-    input logic RSTN,
-    input logic SERIAL_RX,
-    output logic [DATA_WIDTH-1:0] DOUT
+    input logic CLK_I,
+    input logic RST_I,
+    input logic SERIAL_RX_I,
+    output logic BUSY_O,
+    output logic [DATA_WIDTH-1:0] DATA_O
 );
   typedef enum {
-    RESET_RX,
     IDLE_RX,
     START_RX,
     DATA_RX,
     STOP_RX
   } fsm_state;
+
   fsm_state state;
-  fsm_state next_state;
+  logic serial_rx_q;
+  logic serial_rx_qq;
+  logic busy;
   // index of TX DATA
-  int unsigned index;
+  logic [$clog2(DATA_WIDTH)-1:0] index;
   // clock cycle count 
-  int unsigned cycle_cnt;
+  logic [$clog2(CLKS_PER_BIT)-1:0] cycle_cnt;
   logic [DATA_WIDTH-1:0] shift_reg;
-  logic index_cntr_en;
-  logic cycle_cntr_en;
-  logic shift_en;
-  
-  always_comb begin
-    index_cntr_en = 0;
-    cycle_cntr_en = 0;
-    shift_en = 0;
-    next_state = state;
-    case (state)
-      RESET_RX: begin
-        next_state = IDLE_RX;
-      end
-      IDLE_RX: begin
-        if (SERIAL_RX == 1'b0) next_state = START_RX;
-      end
-      START_RX: begin
-        cycle_cntr_en = 1;
-        if (SERIAL_RX == 1'b1) begin
-          next_state = IDLE_RX;
-        end else if (cycle_cnt >= (((CLKS_PER_BIT - 1) / 2) - 1)) begin
-          next_state = DATA_RX;
-          cycle_cntr_en = 0;
-        end
-      end
-      DATA_RX: begin
-        cycle_cntr_en = 1;
-        index_cntr_en = 1;
-        if (cycle_cnt >= (CLKS_PER_BIT - 1)) begin
-          shift_en = 1;
-          if (index >= (DATA_WIDTH - 1)) begin
-            cycle_cntr_en = 0;
-            next_state = STOP_RX;
+  logic baud_tick;
+  logic baud_tick_half;
+  logic index_done;
+
+  always_ff @(posedge CLK_I) begin
+    if (RST_I) begin
+      serial_rx_q  <= 1;
+      serial_rx_qq <= 1;
+    end else begin
+      serial_rx_q  <= SERIAL_RX_I;
+      serial_rx_qq <= serial_rx_q;
+    end
+  end
+
+  always_ff @(posedge CLK_I) begin
+    if (RST_I) begin
+      state <= IDLE_RX;
+      busy <= 0;
+      cycle_cnt <= 0;
+      index <= 0;
+      shift_reg <= 0;
+    end else begin
+      case (state)
+        IDLE_RX: begin
+          cycle_cnt <= 0;
+          busy <= 0;
+          if (serial_rx_qq == 1'b0) begin
+            state <= START_RX;
+            cycle_cnt <= 0;
+            busy  <= 1;
           end
         end
-      end
-      STOP_RX: begin
-        cycle_cntr_en = 1;
-        if (cycle_cnt >= (CLKS_PER_BIT - 1)) next_state = IDLE_RX;
-      end
-      default: begin
-        next_state = IDLE_RX;
-      end
-    endcase
-  end
-  always_ff @(posedge CLK) begin
-    if (!RSTN) begin
-      state <= RESET_RX;
-    end else begin
-      state <= next_state;
+        START_RX: begin
+          cycle_cnt <= cycle_cnt + 1;
+          if (serial_rx_qq == 1'b1) begin
+            state <= IDLE_RX;
+            cycle_cnt <= 0;
+            busy <= 0;
+            // find the middle of the bit
+          end else if (baud_tick_half) begin
+            state <= DATA_RX;
+            index <= 0;
+            cycle_cnt <= 0;
+          end
+        end
+        DATA_RX: begin
+          cycle_cnt <= cycle_cnt + 1;
+          if (baud_tick) begin
+            cycle_cnt <= 0;
+            shift_reg <= {serial_rx_qq, shift_reg[7:1]};
+            index <= index + 1;
+            if (index_done) begin
+              cycle_cnt <= 0;
+              index <= 0;
+              state <= STOP_RX;
+            end
+          end
+        end
+        STOP_RX: begin
+          cycle_cnt <= cycle_cnt + 1;
+          if (baud_tick) begin
+            state <= IDLE_RX;
+            busy  <= 0;
+            cycle_cnt <= 0;
+          end
+        end
+      endcase
     end
   end
 
-  // index counter
-  // incerements every time cycle cnt reaches its max count value
-  always_ff @(posedge CLK) begin
-    if (!RSTN || !index_cntr_en) begin
-      index <= 0;
-    end else begin
-      if (cycle_cnt >= (CLKS_PER_BIT - 1)) index <= index + 1;
-    end
-  end
-
-  // clock cycle counter
-  // increments every time it reaches the number of clk cycles per bit,
-  // which is selected based on the desired baud rate and source clock frequency
-  always_ff @(posedge CLK) begin
-    if (!RSTN || !cycle_cntr_en) begin
-      cycle_cnt <= 0;
-    end else begin
-      if (cycle_cnt >= (CLKS_PER_BIT - 1)) cycle_cnt <= 0;
-      else cycle_cnt <= cycle_cnt + 1;
-    end
-  end
-
-  // shift register for dout
-  // shifts the register by one bit for every clock it is enabled,
-  // shifting in the serial rx input.
-  always_ff @(posedge CLK) begin
-    if (!RSTN) begin
-      shift_reg <= 0;
-    end else if (shift_en) begin
-      shift_reg <= {SERIAL_RX, shift_reg[7:1]};
-    end
-  end
-
-  assign DOUT = shift_reg;
+  assign baud_tick = (cycle_cnt == (CLKS_PER_BIT - 1));
+  assign baud_tick_half = (cycle_cnt == (((CLKS_PER_BIT - 1) / 2) - 1));
+  assign index_done = (index >= DATA_WIDTH - 1);
+  assign BUSY_O = busy;
+  assign DATA_O = shift_reg;
 
   /******************************************/
   //
@@ -120,21 +111,35 @@ module uart_rx #(
   //
   /******************************************/
 `ifdef FORMAL
-  initial assume (RSTN == 0);
-  int f_cycle_count;
-  initial f_cycle_count = 0;
-  always_ff @(posedge CLK) begin
-    f_cycle_count <= f_cycle_count + 1;
-    if (f_cycle_count >= 1) assume property (@(posedge CLK) disable iff (!RSTN) ($stable(SERIAL_RX)));
-    if (f_cycle_count >= 3) f_cycle_count <= 0;
+  initial assume (RST_I == 1);
+
+  // Assume clk is your simulation clock
+  logic [$clog2(CLKS_PER_BIT)-1:0] stable_cntr;
+
+  always_ff @(posedge CLK_I) begin
+    if (RST_I)  // tx_q is previous value
+      stable_cntr <= 0;
+    else if (stable_cntr >= CLKS_PER_BIT-1)
+      stable_cntr <= 0;
+    else
+      stable_cntr <= stable_cntr + 1;
   end
+
+  // SVA: allow change only if stable for >= 4 cycles
+  assume property (@(posedge CLK_I) disable iff (RST_I) $changed(
+      SERIAL_RX_I
+  ) |-> (stable_cntr == 0));
+
+  assert property (@(posedge CLK_I) disable iff (RST_I) cycle_cnt <= (CLKS_PER_BIT - 1));
+
+
   // the serial input should hold stable for the expected baud rate
   // assume baud rate requires 4 clocks per bit
-  successful_receive:
-  cover property (@(posedge CLK) disable iff (!RSTN) 
-  (DOUT == 8'hda ));
-  idle_back_to_idle:
-  cover property (@(posedge CLK) disable iff (!RSTN) 
+  cover property (@(posedge CLK_I) disable iff (RST_I) (DATA_O == 8'hda));
+  cover property (@(posedge CLK_I) disable iff (RST_I) 
   (state == IDLE_RX) ##[1:$] (state == START_RX)  ##[1:$] (state == DATA_RX) ##[1:$] (state == STOP_RX) ##[1:$] (state == IDLE_RX));
+
+  assert property (@(posedge CLK_I) (!RST_I && $past(RST_I) |-> (state == IDLE_RX)));
 `endif
 endmodule
+
--- rtl/uart_tx.sv
+++ rtl/uart_tx.sv
@@ -7,93 +7,124 @@ module uart_tx #(
     // baud rate
     parameter CLKS_PER_BIT = 87
 ) (
-    input logic CLK,
-    input logic RSTN,
-    input logic START,
-    input logic [DATA_WIDTH-1:0] din,
-    output logic SERIAL_TX,
-    output logic DONE,
+    input logic CLK_I,
+    input logic RST_I,
+    input logic START_I,
+    input logic [DATA_WIDTH-1:0] DIN_I,
+    output logic SERIAL_TX_O,
+    output logic BUSY_O
 );
   // FSM states
-  enum int unsigned {
+  typedef enum {
     IDLE_TX,
     START_TX,
     DATA_TX,
     STOP_TX
-  } state;
-  // index of TX DATA
-  int unsigned index;
-  // clock cycle count 
-  int unsigned cnt_clock;
+  } state_t;
 
-  always_ff @(posedge CLK) begin
-    if (!RSTN) begin
+  state_t state;
+
+  logic busy;
+  logic serial_tx;
+  // index of TX DATA
+  logic [$clog2(DATA_WIDTH)-1:0] index;
+  // clock cycle count 
+  logic [$clog2(CLKS_PER_BIT)-1:0] cycle_cnt;
+  logic baud_tick;
+  logic index_last;
+  logic [DATA_WIDTH-1:0] din_q;
+
+  always_ff @(posedge CLK_I) begin
+    if (RST_I) begin
       state <= IDLE_TX;
+      busy <= 0;
+      index <= 0;
+      cycle_cnt <= 0;
+      serial_tx <= 1;
     end else begin
       case (state)
         IDLE_TX: begin
-          //output
-          SERIAL_TX <= 1'b1;
-          index <= 0;
-          cnt_clock <= 0;
-          DONE <= 0;
-          //next state
-          if (START) state <= START_TX;
-          else state <= IDLE_TX;
+          serial_tx <= 1'b1;
+          busy <= 0;
+          if (START_I) begin
+            state <= START_TX;
+            cycle_cnt <= 0;
+            busy <= 1;
+            din_q <= DIN_I;
+          end
         end
         START_TX: begin
-          //output
-          SERIAL_TX <= 1'b0;
-          index <= 0;
-          cnt_clock <= cnt_clock + 1;
-          DONE <= 0;
-          //next state
-          if (cnt_clock < CLKS_PER_BIT - 1) state <= START_TX;
-          else begin
-            cnt_clock <= 0;
+          serial_tx <= 1'b0;
+          cycle_cnt <= cycle_cnt + 1;
+          if (baud_tick) begin
             state <= DATA_TX;
+            cycle_cnt <= 0;
+            serial_tx <= din_q[0];
+            index <= 0;
           end
         end
         DATA_TX: begin
-          //output
-          SERIAL_TX <= din[index];
-          DONE <= 0;
-          //next state
-          if (cnt_clock < (CLKS_PER_BIT - 1)) begin
-            cnt_clock <= cnt_clock + 1;
-          end else begin
-            cnt_clock <= 0;
-            if (index < (DATA_WIDTH - 1)) begin
-              index <= index + 1;
-            end else begin
-              index <= 0;
+          serial_tx <= din_q[index];
+          cycle_cnt <= cycle_cnt + 1;
+          if (baud_tick) begin
+            serial_tx <= din_q[index + 1];
+            index <= index + 1;
+            cycle_cnt <= 0;
+            if (index_last) begin
               state <= STOP_TX;
+              cycle_cnt <= 0;
+              index <= 0;
+              serial_tx <= 1'b1;
             end
+            
           end
         end
         STOP_TX: begin
-          //output
-          SERIAL_TX <= 1'b1;
-          index <= 0;
-          cnt_clock <= cnt_clock + 1;
-          DONE <= 0;
-          //next state
-          if (cnt_clock < CLKS_PER_BIT - 1) state <= STOP_TX;
-          else begin
+          serial_tx <= 1'b1;
+          cycle_cnt <= cycle_cnt + 1;
+          if (baud_tick) begin
             state <= IDLE_TX;
-            DONE  <= 1'b1;
+            serial_tx <= 1'b1;
+            cycle_cnt <= 0;
+            index <= 0;
+            busy <= 0; 
           end
-        end
-        default: begin
-          SERIAL_TX <= 1'b1;
-          index <= 0;
-          cnt_clock <= 0;
-          DONE <= 0;
-          state <= IDLE_TX;
         end
       endcase
     end
   end
+  assign index_last = (index >= DATA_WIDTH-1);
 
+  assign baud_tick = (cycle_cnt == (CLKS_PER_BIT - 1));
+  
+  assign SERIAL_TX_O = serial_tx;
+  assign BUSY_O = busy;
+
+  /******************************************/
+  //
+  //    FORMAL VERIFICATION
+  //
+  /******************************************/
+`ifdef FORMAL
+  initial assume (RST_I == 1);
+  assume property (@(posedge CLK_I) disable iff (RST_I) 
+  busy |-> !START_I);
+
+  // the tx signal should stay stable for N clock cycles, but can change before starting
+  assert property (@(posedge CLK_I) disable iff (RST_I) 
+  !(($past(state) == IDLE_TX && state == START_TX)) && !baud_tick |-> ##1 $stable(SERIAL_TX_O));
+
+  // counter should never exceed CLKS_PER_BIT - 1
+  assert property (@(posedge CLK_I) disable iff (RST_I) 
+  cycle_cnt <= (CLKS_PER_BIT-1));
+
+  // the serial input should hold stable for the expected baud rate
+  // assume baud rate requires 4 clocks per bit
+  cover property (@(posedge CLK_I) disable iff (RST_I) 
+  (state == IDLE_TX) ##[1:$] (state == START_TX)  ##[1:$] (state == DATA_TX) ##[1:$] (state == STOP_TX) ##[1:$] (state == IDLE_TX));
+
+  // reset should return to the idle state
+  assert property (@(posedge CLK_I) (!RST_I && $past(RST_I) |-> (state == IDLE_TX)));
+`endif
 endmodule
 
